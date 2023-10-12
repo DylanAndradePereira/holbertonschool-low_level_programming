ae9e9a5ac54e7812cd8e4aadf907214ed64d7e05
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-12 06:00:57 -0700

    jfijdpi

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - This is a description
+  * Return: Description of the returned value
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
