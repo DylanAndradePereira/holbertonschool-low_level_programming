6ff7f5d05ab6b40d326f771f114208e73738ef1a
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-12 06:36:05 -0700

    énifdp

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 int main(void)
-{
-	int integerType;
-	float floatType;
-	double doubleType;
+{        
 	char charType;
+        int integerType;
+        long int longType;
+        long long int longlongType
+        float floatType;
 
-	// Sizeof operator is used to evaluate the size of a variable
-	printf("Size of int: %ld bytes\n",sizeof(integerType));
-	printf("Size of float: %ld bytes\n",sizeof(floatType));
-	printf("Size of double: %ld bytes\n",sizeof(doubleType));
+
 	printf("Size of char: %ld byte\n",sizeof(charType));
+	printf("Size of int: %ld bytes\n",sizeof(integerType));
+        printf("Size of float: %ld bytes\n",sizeof(longType));
+        printf("Size of float: %ld bytes\n",sizeof(longlongType));
+        printf("Size of float: %ld bytes\n",sizeof(floatType));
 
-	return 0;
+        return 0;
 }
