44c2a3a311f7f31f39e270d93a4e26ffe2ab39b8
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-12 06:18:25 -0700

    fdnio

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - This is a description
+  * Return: Description of the returned value
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
