4cefc13cc0b7701a51e8cdd438ccd24c78b6181b
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-16 04:43:31 -0700

    fd

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,16 +20,16 @@ int main(void)
 	if (last > 0)
 	{
 		/* affiche 0 is zero */
-		printf("Last digit of %d is %d and is is greater than 5\n", n, last);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
 	else if (last > 5)
 	{
 		/* affiche n is positive */
-		printf("Last digit of %d is %d and is is greater than 5\n", n, last);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	} else if (last < 6 && last != 0)
 	{
 		/* affiche n is negative */
-		printf("Last digit of %d is %d and is is greater than 5\n", n, last);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
 	return (0);
 }
