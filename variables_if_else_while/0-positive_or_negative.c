401010d9570e4a9c5fabd201990c595cc284020b
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-16 03:21:54 -0700

    fdni

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,30 @@
 #include <stdio.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
+/**
+ * Function main
+ * @return the sum of a and b
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if(n==0)
+	/* check the boolean condition */
+	if(n == 0)
 	{
-		printf("%d is zero\n",n);
+		/* affiche 0 is zero */
+		printf( "%d is zero\n", n);
 	}
-	else if(n>0)
+	else if(n > 0)
 	{
-		printf("%d is positive\n",n);	
+		/* affiche n is positive */
+		printf( "%d is positive\n", n);	
 	}else
 	{
-		printf("%d is negative\n",n);
+		/* affiche n is negative */
+		printf( "%d is negative\n", n);
 	}
 	return (0);
 }
