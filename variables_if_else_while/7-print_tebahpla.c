df2aed515376ec4221130c96f05bca0114711b9e
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-16 06:48:12 -0700

    fjid

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Check a number
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122 ; i > 96 ; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
