aaf446b0908a5699a4736875ec63ca47e09f9076
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-16 05:12:01 -0700

    fdi

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Check a number
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 61 ; i > 87 ; i++)
+	{
+		putchar(i);
+	}
+
+	return (0);
+}
