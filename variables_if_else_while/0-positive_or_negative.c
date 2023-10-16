1e1fd9e3c6b96bf2060b0c757778b1e66bf41477
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-16 04:12:05 -0700

    fdhi

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,36 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /* more headers goes there */
+
 /* betty style doc for function main goes there */
+
 /**
- * Function main
- * @return the sum of a and b
+ * main - Check if a number is greater than 0
+ * @n: The number to be checked
+ *
+ * Return: 0
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* check the boolean condition */
-	if(n == 0)
+	if (n == 0)
 	{
 		/* affiche 0 is zero */
-		printf( "%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
-	else if(n > 0)
+	else if (n > 0)
 	{
 		/* affiche n is positive */
-		printf( "%d is positive\n", n);	
-	}else
+		printf("%d is positive\n", n);
+	} else
 	{
 		/* affiche n is negative */
-		printf( "%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
