3f5aec5152374b1b730450ddb7d5487d3ea80868
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-16 07:17:46 -0700

    fjdo

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Check a number
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		putchar(i);
+		if(i != 57)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
