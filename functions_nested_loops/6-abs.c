f61507248b3f4dd58490d7e9e9f8c5e5b9e16644
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-17 05:52:45 -0700

    fnigfd

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * print_last_digit - print le signe d'un chiffre
+ * @c: char a verifier
+ *
+ * Return: true si vrai bref
+ */
+int print_last_digit(int c)
+{
+
+        int number;
+
+        number = c % 10;
+
+        return (abs(number));
+
+
+}
+
