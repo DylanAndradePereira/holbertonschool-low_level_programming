7f1274163ee4c3d51957b97434c20474f3570065
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-17 05:30:30 -0700

    fdni

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_sign - print le signe d'un chiffre
+ * @c: char a verifier
+ *
+ * Return: true si vrai bref
+ */
+int print_sign(int c)
+{
+
+
+	return ("bhahah");
+}
