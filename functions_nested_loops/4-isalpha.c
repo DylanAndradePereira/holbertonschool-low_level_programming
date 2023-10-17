7f756a494a1611b368640675db74c9204e36a239
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-17 05:23:34 -0700

    cdi

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+
+/**
+ * _isalpha - verifie si c'est alpha 
+ * @c: char a verifier
+ *
+ * Return: true si vrai bref
+ */
+int _isalpha(int c)
+{
+	        int verif;
+
+	int verif;
+
+	if (isalpha(c) == 0)
+	{
+			verif = 0;
+	}
+	else
+	{
+			verif = 1;
+	}
+	return (verif);
+
+}
