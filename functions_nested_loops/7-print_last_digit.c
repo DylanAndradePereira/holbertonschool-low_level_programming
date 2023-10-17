f5a98ba250cc9041026588f18ce76c52c24dff48
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-17 05:48:04 -0700

    di

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <inttypes.h>
+
+/**
+ * print_sign - print le signe d'un chiffre
+ * @c: char a verifier
+ *
+ * Return: true si vrai bref
+ */
+int _abs(int c)
+{
+
+	int number;
+
+	number = c % 10;
+
+	return (abs(number));
+
+
+}
