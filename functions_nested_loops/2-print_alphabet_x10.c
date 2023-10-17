6bb38f92e1c2768d809a7dad8cf3a92f12e03777
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-17 04:46:18 -0700

    fdji

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
-
 /**
  * print_alphabet_X10 - affice l'alphabet
  *
  * Return: l'alphabet dix fois
  */
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int i;
 	int j;
+
 	for (j = 0 ; j < 10 ; j++)
 	{
 		for (i = 97 ; i < 123 ; i++)
-        	{
+		{
 			_putchar(i);
-        	}
+			_putchar('\n');
+		}
 	}
-	
+
 }
