4f0aa4d71dcb6024fcc3f9b9ffc216fb0d5a7917
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-17 00:49:38 -0700

    fdi

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Check a number
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar \n");
+	return (0);
+}
