8cd595607f340f8008cc4ffbdca9eb0385681360
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-18 05:55:21 -0700

    fdnhi

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - check the code for Holberton School students.
+ * jack_bauer - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
