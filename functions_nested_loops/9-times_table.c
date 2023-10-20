87a1ba0add8a3858b626359d4e06e545d2ced3ad
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-20 04:51:31 -0700

    fdni

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * times_table - check the code for Holberton School students.
+ *
+ */
+void times_table(void)
+{
+	int t;
+	int i = 0;
+	int f;
+
+	for (f = 0; f < 10; f++)
+	{
+		t = 0;
+		putchar('\n');
+		for (t = 0; t < 10; t++)
+		{
+			int r = f*t;
+			printf("%d",r);
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	
+}
