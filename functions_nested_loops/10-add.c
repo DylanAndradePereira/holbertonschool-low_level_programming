15f7dc572f903f8d388b2213d904f3776a6587f8
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-20 05:26:02 -0700

    fdhi

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add - check the code for Holberton School students.
+ *
+ * @un : param 1
+ * @deux : param 2
+ * 
+ * Return: l'addition des deux chiffre en parametre.
+ */
+int add(int un, int deux)
+{
+
+	return (un + deux);
+
+}
+
+
