031a6a75f70fc1f40d6b23e3e8dd4ad08daa2e19
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-20 05:30:32 -0700

    fdjo

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_to_98 - check the code for Holberton School students.
+ *
+ * @un : param 1
+ */
+void print_to_98(int un)
+{
+
+	int i;
+	for ( i = un; i < 98; i++)
+	{
+		printf("%d", i);
+	}
+	
+
+}
