bea0cc0618f57a078b800694e8f672bcb6fc2880
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-20 05:03:33 -0700

    dj

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,19 +10,28 @@ void times_table(void)
 	int t;
 	int f;
 
-	for (f = 0; f < 10; f++)
-	{
-		t = 0;
-		putchar('\n');
-		for (t = 0; t < 10; t++)
+		for (f = 0; f < 10; f++)
 		{
-			int r = f*t;
-			printf("%d",r);
-			putchar(',');
-			putchar(' ');
+			t = 0;
+			putchar('\n');
+			for (t = 0; t < 10; t++)
+			{
+				int r = f*t;
+				printf("%d",r);
+
+				if (t!=9)
+				{
+					/* code */
+					putchar(',');
+				}
+
+
+				putchar(' ');
+				if (r<10)
+				{
+					putchar(' ');
+				}
+			}
+
 		}
-
-	}
-
-	
 }
