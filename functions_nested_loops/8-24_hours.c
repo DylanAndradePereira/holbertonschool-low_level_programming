76fb2a115ab153ad8031d0fedf6ddbc71aaf9abc
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-22 02:46:25 -0700

    fji

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,31 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * jack_bauer - check the code for Holberton School students.
+ * jack_bauer - Affiche l'heure de la journée entiere
  *
  * Return: Always 0.
  */
 void jack_bauer(void)
 {
-	int t;
-	int i = 0;
-	int f;
+	int i, j;
+	int jour = 0;
 
-	for (f = 0; f < 24; f++)
+	for (j = 0; j < 24; j++)
 	{
-		for (t = 0; t <= 59; t++)
+		for (i = 0; i < 60; i++, jour++)
 		{
-			/* 24 fois */
-
-			printf("%02d:%02d\n", i, t);
-
-			if (t == 59)
-			{
-				i++;
-			}
-
+			if (jour == 1440)
+				break;
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar(':');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar('\n');
 		}
-
 	}
-
 }
+
