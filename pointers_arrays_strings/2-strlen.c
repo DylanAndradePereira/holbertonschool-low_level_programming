b80b8bae5321550f9f7e5b2a72a3ecdd06eefb72
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-23 04:42:39 -0700

    fnjdfo

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - entry point
+ *
+ * Description: Prints diagonals
+ *@adresse1: size of the triangle
+ *@adresse2: dhufdhnu
+ * Return: void
+ */
+
+void swap_int(char str)
+{
+
+	int n = 0;
+
+	while ( s[n] ) ++n;
+
+	return (n)
+}
