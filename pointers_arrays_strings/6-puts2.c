f77637590c8942f61885a5a66df9d3ebb62e9eb3
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-23 09:54:07 -0700

    dnio

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - entry point
+ * Description: tu connais flemme un peu
+ * @str: poiteurde la chaine a compter
+ * Return: void
+ */
+
+void (char *str)
+{
+	for (i = 0; i < strlen(str); i++)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
