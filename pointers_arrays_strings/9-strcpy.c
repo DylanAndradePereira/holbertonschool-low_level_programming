58cb6cb2a1937d94f59a6a3ba3c1a81ef5dd10dc
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-24 04:35:46 -0700

    fdio

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	long unsigned int i;
 
 	for (i = 0; i < strlen(src); i++)
 	{
 		dest[i] = src[i];
 		putchar(dest[i]);
 	}
+
+	return (dest);
 }
