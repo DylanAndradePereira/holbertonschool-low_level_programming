b8a7d58b04f07965bf488a95d4e6790169215ca2
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-24 14:10:21 -0700

    fdpi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,7 @@ int _atoi(char *str)
 		if (str[i] >= '0' && str[i] <= '9')
 		{
 			if (symbole != ' ' && str[i - 1] == '-')
-				symbole = str[i - 1];
+				putchar('p');
 
 			result = result*10 + str[i] - '0';
 		}
