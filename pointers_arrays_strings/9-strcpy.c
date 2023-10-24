b242f549531a3ebe0fa0aa7e5ee2ec247fd9d961
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-24 04:33:00 -0700

    nfdi

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - entry point
+ * Description: tu connais flemme un peu
+ * @ui: poiteurde la chaine a compteri
+ * @array: strafoulah
+ * Return: void
+ */
+
+void _strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < strlen(src); i++)
+	{
+		dest[i] = src[i];
+		putchar(dest[i]);
+	}
+}
