2bb121ffbf3cdd48e3aca8e55d404d6bf87fd4c4
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-24 06:51:17 -0700

    fdniog

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _atoi - entry point
+ * Description: tu connais flemme un peu
+ * @string: poiteurde la chaine a compteri
+ * Return: mixed value llolololol
+ */
+
+char _atoi(char *string)
+{
+
+	return (int(string));
+}
