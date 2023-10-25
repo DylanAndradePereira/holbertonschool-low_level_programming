5f752c55219bc7f2c1a665285da9b08fba53b3e2
DylanAndradePereira <dylan@dylan-andrade-pereira.fr>
2023-10-25 02:00:57 -0700

    fdni

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -15,12 +15,18 @@ int _atoi(char *str)
 
 	unsigned result = 0;
 
-
+	bool verif;
 
 	for (i = 0;i < strlen(str);i++)
 	{
 		if (str[i] >= '0' && str[i] <= '9')
 		{
+		
+			if ((symbole != ' ' && str[i - 1] == '-') || (symbole != ' ' && str[i -1] == ' '))
+        		{
+                		symbole = '-';
+        		}
+		
 			result = result*10 + str[i] - '0';
 		}
 
